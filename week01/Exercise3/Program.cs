5093c4fadfa788055f741e89e7efc73430976c8b
KingKacrae99 <kacrae9@gmail.com>
2025-03-14 15:43:49 +0100

    Exercise 3 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        string response = "yes";
+
+        while (response.ToLower() == "yes"){
+
+            int magicNumber = randomGenerator.Next(1,100);
+            int number = 0;
+            int attempts = 0;
+
+            while(number != magicNumber ){
+
+                Console.WriteLine("What is the magic number? ");
+                string userInput = Console.ReadLine();
+                number = int.Parse(userInput);
+                attempts += 1;
+
+                if (number > magicNumber){
+                    Console.WriteLine("Lower");
+                }
+                else if (number < magicNumber){
+                    Console.WriteLine("Higher");
+                }
+                
+            }
+
+            Console.WriteLine($"You guessed right! attempt: {attempts} times");
+            Console.Write("Do you want to play again. (yes or no)? ");
+            response = Console.ReadLine();
+       }
     }
 }
