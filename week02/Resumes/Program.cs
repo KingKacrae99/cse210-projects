6600d3e77eda84359daf97b45f703a974f4a6f3c
KingKacrae99 <kacrae9@gmail.com>
2025-03-18 22:31:35 +0100

    Resume Activity Completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "microsoft";
+        job1._jobTitle ="Software Engineer";
+        job1._startYear = 2024;
+        job1._endYear = 2026;
+
+        Job job2 = new Job();
+        job2._company = "Open AI";
+        job2._jobTitle = "AI Developer";
+        job2._startYear = 2025;
+        job2._endYear = 2027;
+
+        // Resume instance
+        Resume myResume = new Resume();
+        myResume._name = "King Kacrae";
+        myResume._job.Add(job1);
+        myResume._job.Add(job2);
+
+        myResume.Display();
+        //job2.DisplayJobDetails();
     }
 }
