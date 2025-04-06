e3a9c6691af45f057754ed41f363189c715d76fe
KingKacrae99 <kacrae9@gmail.com>
2025-04-06 03:28:00 +0100

    Assignment completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("John", "programming with Java");
+        MathAssignment maths = new MathAssignment("Roberto","Fractions","7.3","8-19");
+        WritingAssignment written= new WritingAssignment("Mary waters","Historology of Christianity","Reign of the Roman Empire");
+
+        Console.WriteLine(maths.GetSummary());
+        Console.WriteLine(maths.GetHomeworkList());
+        Console.WriteLine("");
+        Console.WriteLine(written.GetSummary());
+        Console.WriteLine(written.GetWritingInformation());
     }
 }
